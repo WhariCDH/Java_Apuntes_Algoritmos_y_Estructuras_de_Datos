38e8f148c4a32d6a6bf6a806286aca2419dd4596
WhariCDH <huaripatagarcia@gmail.com>
2026-03-31 12:16:50 -0500

    anime

--- Semana06_Practica/GuardadoH.java
+++ Semana06_Practica/GuardadoH.java
@@ -3,44 +3,14 @@ import java.util.ArrayList;
 public class GuardadoH {
 
     public static void main(String[] args) {
-        ArrayList<String> Hentay=new ArrayList<>();
+        ArrayList<String> anime=new ArrayList<>();
 
-        Hentay.add("True B");
-        Hentay.add("Shigokare Ecchi na Joshi Daisei to Doki x2 Love Lesson");
-        Hentay.add("Ryoujoku Famiresu Choukyou Menu ");
-        Hentay.add("Rennyuu Tales The Animation");
-        Hentay.add("Real E S! TAni");
-        Hentay.add("Pinkerton");
-        Hentay.add("Niizuma Koyomi The Animation");
-        Hentay.add("Nariyuki Papakatsu Girls!! The Animation");
-        Hentay.add("Mou Hasamazu ni wa Irarenai Hoshi ni Onegai Shitara Konna ni Okkiku Nacchatta!");
-        Hentay.add("Kimi no Mana wa Rina Witch ");
-        Hentay.add("Juvenile Pornography The Animation");
-        Hentay.add("Iizuka-senpai x Blazer Ane Kyun! yori The Animation");
-        Hentay.add("Hatsujou Switch Otosareta Shoujo-tachi The Animation");
-        Hentay.add("Hakoiri Shoujo - Virgin Territory");
-        Hentay.add("Furueru Kuchibiru");
-        Hentay.add("Fella Pure The Animation");
-        Hentay.add("Betsuni Anta no Tame ni Ookiku Nattan Janain Dakarane!!");
-        Hentay.add("Baku Ane 2 Otouto Ippai Shibocchau zo! The Animation");
-        Hentay.add("Anoko to Iikoto");
-        Hentay.add("Iinari! Saimin Kanojo");
-        Hentay.add("Koiito Kinenbi The Animation");
-        Hentay.add("Machi Gurumi no Wana");
-        Hentay.add("Rasen Sokou no Dystopia");
-        Hentay.add("S H H na Oneesan wa S D ka");
-        Hentay.add("Kagirohi Shaku Kei");
-        Hentay.add("Toriko no Kusari ");
-        Hentay.add("Uhou Renka");
-        Hentay.add("Hump Bang!");
-        Hentay.add("Honoo no Haramase Oppai Ero Appli Gakuen The Animation ");
-        Hentay.add("Joshikousei no Koshitsuki");
-        Hentay.add("Anata wa Watashi no Mono");
-
+        anime.add("One piece");
+        
         System.out.println("Anime en listas para descarga.");
         int i=1;
 
-        for (String descarga : Hentay){
+        for (String descarga : anime){
             System.out.println( +i+ ". Animes por descargar: " +descarga);
             i++;
         }
